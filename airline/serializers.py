f793601d744a0dc66f0d4b0f0f1fc68a024a198a
Vip247-cloud <vipinglb@gmail.com>
2025-03-30 03:28:29 +0530

    airline_booking_backend

--- airline/serializers.py
+++ airline/serializers.py
@@ -14,7 +14,19 @@ class PassengerSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+# class BookingSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Booking
+#         fields = '__all__'
+
 class BookingSerializer(serializers.ModelSerializer):
+    flight_number = serializers.CharField(source='flight.flight_number', read_only=True)
+    passenger_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = ['id', 'seat_number', 'booking_date', 'status', 'flight', 'flight_number', 'passenger', 'passenger_name']
+
+    def get_passenger_name(self, obj):
+        return f"{obj.passenger.first_name} {obj.passenger.last_name}"
+
